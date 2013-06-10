0c33049bf76f0d259b3593225316b7813b4a51f4
buliwyf42 <dominik@sennfelder.de>
2013-06-10 14:55:46 +0200

    fixed bugs

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ dotfiles
 
 Collection of various config files
 
+## Install
+
+run ```git clone --recursive https://github.com/buliwyf42/dotfiles``` to clone with all submodules
+
+
 ## Vim
 
 run ```git submodule init && git submodule update``` before starting vim
--- makesysmlinks.sh
+++ makesysmlinks.sh
@@ -17,28 +17,19 @@ echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done
 
 function install_zsh {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
-    fi
     # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+    if [[ ! $(grep $USER /etc/passwd | awk  -F ':' {' print $NF}') == $(which zsh) ]]; then
         chsh -s $(which zsh)
     fi
 else
